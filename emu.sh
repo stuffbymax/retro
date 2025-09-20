68493e3e5df8cdac72ade5f941d0763011d34866
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-09-20 19:17:00 +0100

    Update emu.sh

--- emu.sh
+++ emu.sh
@@ -6,21 +6,21 @@ echo "Updating pkgin package database..."
 sudo pkgin update
 
 echo "Installing PC & DOS emulators..."
-sudo pkgin install -y qemu bochs dosbox 8086tiny tme applyppf
+sudo pkgin install qemu bochs dosbox 8086tiny tme applyppf
 
 echo "Installing Console emulators..."
-sudo pkgin install -y bsnes snes9x fceux genesis-plus-gx mednafen ppsspp pcsx2 dolphin cygne-sdl basiliskII raine vecx
+sudo pkgin install bsnes snes9x fceux genesis-plus-gx mednafen ppsspp pcsx2 dolphin cygne-sdl basiliskII raine vecx
 
 echo "Installing Home computer emulators..."
-sudo pkgin install -y atari800 aranym arcem xbeeb b-em vice arnold
+sudo pkgin install atari800 aranym arcem xbeeb b-em vice arnold
 
 echo "Installing Arcade & Retro front-end emulators..."
-sudo pkgin install -y advancemame blastem cannonball blinkensim raine emulationstation
+sudo pkgin install advancemame blastem cannonball blinkensim raine emulationstation
 
 echo "Installing RetroArch and all available cores..."
-sudo pkgin install -y retroarch retroarch-cores/*
+sudo pkgin install retroarch retroarch-cores/*
 
 echo "Upgrading all installed packages to latest versions..."
-sudo pkgin upgrade -y
+sudo pkgin upgrade
 
 echo "All emulators, RetroArch, and RetroArch cores have been installed and updated!"
